229e81b4322dde6781503939d2defbd9164e39a4
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-09-27 12:49:24 +0200

    squelette html + mise en page

---
+++ work_exp.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="root.css">
+    <link rel="stylesheet" href="bubble.css">
+    <title>CV de Tintin</title>
+</head>
+<body>
+<!--  ci-dessus à effacer  -->
+
+<link rel="stylesheet" href="./work_exp.css">
+
+
+<div class="works">
+    <div class="work">
+        <div class="job">Jounaliste</div>
+        <div class="tintinId"></div>
+        <div class="bubble">bulle</div>
+    </div>
+</div>
+
+<div class="works">
+    <div class="work">
+        <div class="bubble">bulle</div>
+        <div class="tintinId"></div>
+        <div class="job">Pilote de sous-marin</div>
+    </div>
+</div>
+
+
+
+<!--  ci-dessous à effacer  -->
+
+</body>
+</html>
