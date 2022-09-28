f4f05b97175c0db71699f834799f961ef965b0cf
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-09-28 15:10:45 +0200

    habillage avec planche bd de la bordure

--- work_exp.php
+++ work_exp.php
@@ -16,43 +16,83 @@
 </head>
 <body>
 <!--  ci-dessus à effacer  -->
-<link href="http://fonts.cdnfonts.com/css/tintin" rel="stylesheet">
-                
+<link href="http://fonts.cdnfonts.com/css/tintin" rel="stylesheet">             
 <link rel="stylesheet" href="./work_exp.css">
 
 
-<div class="works">
+
+<div class="vignettes">
+    <div ></div>
+    <div ></div> 
+    <div ></div>
+
+    <div class="border borderR borderT borderB"></div>
+    <div class="job">Journaliste indépendant
+        <ul>
+            <li>depuis 1929</li>
+            <li>partout dans le monde</li>
+        </ul>
+    </div>
+    <div class="border borderB borderL borderT"></div>
+ 
+    <div class="border borderR borderT borderB planche1"></div>
     <div class="work">
-        <div class="job">Journaliste indépendant</div>
         <div class="tintinId"></div>
-        <div class="bubble speechR">J'ai acquis une certaine facilité à me mêler d'affaires qui ne me regarde pas...</div> </div>
+        <div class="bubble speechR">J'ai acquis une certaine facilité à me mêler d'affaires qui ne me regarde pas...</div> 
+    
     </div>
-</div>
+    <div class="border borderB borderL borderT planche2"></div>
 
-<div class="works">
-    <div class="work">
-            <div class="bubble speechL">Sous-marin, avion , moto, jeep, chameau, fusée...Même sans permis, je le conduis !</div>
-            <div class="tintinId reverse"></div>
-        <div class="job">Pilote en tout genre</div>
+    <div class="border borderR borderT borderB"></div>
+    <div class="job">Pilote en tout genre
+        <ul>
+            <li>date</li>
+            <li>lieu</li>
+        </ul>
     </div>
-</div>
+    <div class="border borderB borderL borderT"></div>
 
-<div class="works">
+    <div class="border borderR borderT borderB planche3"></div>
     <div class="work">
-        <div class="job">Traficant de drogue</div>
+        <div class="bubble speechL">Sous-marin, avion , moto, jeep, chameau, fusée...Même sans permis, je le conduis !</div>
+        <div class="tintinId reverse"></div>
+    </div>
+    <div class="border borderB borderL borderT planche4"></div>
+
+    <div class="border borderR borderT borderB"></div>
+    <div class="job">Traficant de drogue
+        <ul>
+            <li>date</li>
+            <li>lieu</li>
+        </ul>
+    </div>
+    <div class="border borderB borderL borderT"></div>
+
+    <div class="border borderR borderT borderB planche5"></div>
+    <div class="work">
         <div class="tintinId"></div>
         <div class="bubble speechR">J'ai développé mon réseau de contact professionnel jusqu'en Colombie</div>
     </div>
-</div>
+    <div class="border borderB borderL borderT planche6"></div>
 
-<div class="works">
+    <div class="border borderR borderT borderB"></div>
+    <div class="job">Roi Maya
+        <ul>
+            <li>date</li>
+            <li>lieu</li>
+        </ul>
+    </div>
+    <div class="border borderB borderL borderT"></div>
+
+    <div class="border borderR borderT borderB planche1"></div>
     <div class="work">
         <div class="bubble speechL">J'ai développé ma capacité managériale et mon sens de l'autorité.</div>
         <div class="tintinId reverse"></div>
-        <div class="job">Roi Maya</div>
     </div>
-</div>
+    <div class="border borderB borderL borderT planche4"></div>
 
+
+</div>
 <!--  ci-dessous à effacer  -->
 
 </body>
