52f71142dd9e2cc3e4a1d14071971712604c1d9b
lucianpurcaru <lucian.purcaru@gmail.com>
2022-09-28 15:25:00 +0200

    update form on contact.php et contact.css

--- contact.php
+++ contact.php
@@ -10,46 +10,38 @@
 </head>
 
 <body>
-    <section class="contact">
+    <section class="form">
         <h2>Me contacter</h2>
         <p>Pour toute demande me concernant, remplissez le formulaire ci-dessous!</p>
+        <div id="contact_form">
+        <form>
+            <label for="name">Votre Nom</label>
+            <input type="text" id="name" name="fullname" placeholder="Prenom Nom" required="required">
+            <label for="email">Votre mail</label>
+            <input type="text" id="email" name="email" placeholder="example@mail.com" required="required">
+            <label for="subject">Sujet du message</label>
+            <select id="subject">
+                <option value="subject">A propos de...</option>
+                <option value="Une enquête">Une enquête</option>
+                <option value="Un reportage">Un reportage</option>
+                <option value="Un travail">Un travail</option>
+                <option value="Autre chose">Autre chose</option>
+            </select>
+            <label for="message">Votre message</label>
+            <textarea id="message" name="message" required="required"></textarea>
+            <input type="submit" value="Envoyez">
+        </form>
+    </section>
 
-        <div class="contact_form">
-            <form id="contact_form">
-                <label for="name">Votre nom</label></br>
-                <input type="text" id="name" name="name" placeholder="Entrez votre nom" required="required"></br>
-
-                <label for="subject">Sujet de la demande</label></br>
-                <select class="subject"></br>
-                    <option value="subject">Sujet de la demande</option>
-                    <option value="interview">M'interviewer</option>
-                    <option value="job">M'embaucher</option>
-                    <option value="investigation">Une investigation</option>
-                </select></br>
-                <label for="email">Votre email</label></br>
-                <input type="email" id="email" name="email" placeholder="Entrez votre adresse mail" required="required"></br>
-
-                <label for="message">Votre message</label></br>
-                <textarea type="text" id="message" placeholder="Votre message" name="message" required="required"></textarea>
-
-                <input type="submit" value="Envoyer" />
-
-            </form>
-        </div>
-
-        <div class="personal_contact">
-            <h2>Mes coordonnées</h2>
-            <p>L'opportunité de m'embaucher ne se présente pas tous les jours. Voici mes coordonnées afin de me joindre sans plus attendre!</p>
-            <p> Tintin et Milou</p>
-            <p>26, rue du Labrador</p>
-            <p>Bruxelles</p>
-            <p>Téléphone: +32 2 374 00 00</p>
-            <p>Mail: contact@tintinetmilou.be</p>
-
-
+    <section class="adress">
 
     </section>
-    <script src="contact.js"></script>
-</body>
+     
 
+                
+
+       
+
+<script src="contact.js"></script>
+</body>
 </html>
