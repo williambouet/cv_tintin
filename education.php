d23164670928f3ac8c2c6ed0c27ec3c7d914041f
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-09-29 22:42:34 +0200

    formation et experience plus light

---
+++ education.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="root.css">
+    <link rel="stylesheet" href="navbar.css">
+    <link href="http://fonts.cdnfonts.com/css/tintin" rel="stylesheet">
+    <link rel="stylesheet" href="./education.css">
+    <title>CV de Tintin</title>
+</head>
+
+<body>
+
+    <?php include_once('navbar.php') ?>
+
+    <div class="vignettes">
+
+        <div class="firstRow"></div>
+        <div class="firstRow"></div>
+        <div class="firstRow"></div>
+
+
+        <div class="border borderR borderT borderB planche4 case1"></div>
+        <div class="job case2">Formation auprès du Professeur Tournesol
+            <ul>
+                <li>1941</li>
+                <li>France</li>
+            </ul>
+        </div>
+        <div class="border borderB borderL borderT planche3 case3"></div>
+
+        <div class="border borderR borderT borderB planche1 case4"></div>
+        <div class="talking case5">
+            <div class="bubble ">J'ai participé à des inventions pour le concours Lépine, jusqu'à étudier la physique nucléaire avec cet éminent Professeur.</div>
+            <div class="images_tintin">
+                <img src="./img/Tintin_debout.png" class="tintinId" alt="Tintin salue">
+                <img src="" alt=" " class="curious_tintin">
+            </div>
+        </div>
+        <div class="border borderB borderL borderT planche2 case6"></div>
+
+        <div class="border borderR borderT borderB planche5 case7"></div>
+        <div class="job case8">Années sabbatiques
+            <ul>
+                <li>1941</li>
+                <li>Afrique</li>
+            </ul>
+        </div>
+        <div class="border borderB borderL borderT planche6 case9"></div>
+
+        <div class="border borderR borderT borderB planche3 case10"></div>
+        <div class="talking case11">
+            <div class="bubble">J'ai voyagé à travers le monde et appris auprès d'un marin au langage fleurie, à l'haleine chargée, mais par chance son portefeuille le devint également.</div>
+
+            <div class="images_tintin">
+                <img src="./img/Tintin_debout.png" class="tintinId reverse" alt="Tintin salue">
+                <img src="" alt=" " class="curious_tintin">
+             </div>
+
+        </div>
+        <div class="border borderB borderL borderT planche4 case12"></div>
+
+        <div class="border borderR borderT borderB planche2 case13"></div>
+        <div class="job case14">Stage de découverte d'entreprises
+            <ul>
+                <li>1932</li>
+                <li>France</li>
+            </ul>
+        </div>
+
+        <div class="border borderB borderL borderT planche1 case15"></div>
+
+
+        <div class="border borderR borderT borderB planche5 case16"></div>
+        <div class="talking case17">
+            <div class="bubble ">J'ai eu la chance de découvrir le métier de détective privé auprès de deux experts en confusions, malentendus et méprises !</div>
+            <div class="images_tintin">
+                <img src="./img/Tintin_debout.png" class="tintinId" alt="Tintin salue">
+                <img src="" alt=" " class="mushroom">
+            </div>
+        </div>
+        <div class="border borderB borderL borderT planche6 case18"></div>
+
+        <div class="border borderR borderT borderB planche3 case19"></div>
+
+        <div class="job case20">Ecole de la vie
+
+            <ul>
+                <li>1929</li>
+                <li>Belgique</li>
+            </ul>
+        </div>
+        <div class="border borderB borderL borderT planche2 case21"></div>
+
+        <div class="border borderR borderT borderB planche1 case22"></div>
+        <div class="talking case23">
+            <div class="bubble">J'ai énormément appris après mon voyage en union-soviétique. </br> J'ai donc voulu m'essayer à d'autres activités...</div>
+
+            <div class="images_tintin">
+                 <img src="./img/Tintin_debout.png" class="tintinId reverse" alt="Tintin salue">
+                 <img src="" alt=" " class="curious_tintin maya">
+            </div>
+     </div>
+
+        <div class="border borderB borderL borderT planche4 case24"></div>
+
+
+    </div>
+    <!--  ci-dessous à effacer  -->
+
+</body>
+
+</html>
--- read_me.txt
+++ read_me.txt
@@ -12,3 +12,34 @@ chaque fonctionnalité sera une branche ex: navbar a sa branch "navbar"
 Les branches seront mergé sur la proto (pour test), puis sur la main.
 
 --------------------------------------------------------------------------
+
+
+Corrections/Améliorations :
+* > nettoyer son code et mettre ses variables dans root
+
+> NAVBAR :
+ - Acceuil : à passer en Accueil
+ - Border : à passer en angle droit
+ - Logo : à choisir ou disparaître
+ - Ajuster la média quéries (pour passer en menu burger avant le saut de ligne de Exp.Pro)
+ - inverser skills avec Exp.Pro
+
+> Expériences pro :
+ - Vignettes : fermer les bordures des étiquettes intérieures (3 concernées en desktop)
+ - supprimer les 4 traits qui ne servent à rien en bas
+ - ajuster les médias quéries (déclencher le desktop selon les images)
+ - Uniformiser les couleurs des titres
+ - Faire la pointe de bulle avec :before
+ - vérifier les alt
+ - si possible faire des anims sur les objects
+    (requin fait des AR)
+    (statuette comme si elle marchait)
+    (dupontd faire apparaitre la bulle, et je dirais même plus)
+
+> Accueil : 
+ - Diminuer la taille de la font
+ - Réduire la taille des bordures
+ -
+
+> Formation :
+ - Changer la disposition, les couleurs et/ou les images
