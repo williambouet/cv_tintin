881f961bff82c455d97b3e1b46d36a008e5d58d1
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-09-29 09:25:24 +0200

    maj img et grid responsive

--- work_exp.php
+++ work_exp.php
@@ -1,101 +1,100 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link rel="stylesheet" href="root.css">
-    <link rel="stylesheet" href="bubble.css">
-
-
-
-
-                
+    <link rel="stylesheet" href="navbar.css">
+    <link href="http://fonts.cdnfonts.com/css/tintin" rel="stylesheet">
+    <link rel="stylesheet" href="./work_exp.css">
     <title>CV de Tintin</title>
 </head>
+
 <body>
-<!--  ci-dessus à effacer  -->
-<link href="http://fonts.cdnfonts.com/css/tintin" rel="stylesheet">             
-<link rel="stylesheet" href="./work_exp.css">
+
+    <?php include_once('navbar.php') ?>
+
+    <div class="vignettes">
+        <div></div>
+        <div></div>
+        <div></div>
+
+        <div class="border borderR borderT borderB planche4 case1"></div>
+        <div class="job case2">Journaliste indépendant
+            <ul>
+                <li>depuis 1929</li>
+                <li>partout dans le monde</li>
+            </ul>
+        </div>
+        <div class="border borderB borderL borderT planche3 case3"></div>
+
+        <div class="border borderR borderT borderB planche1 case4"></div>
+        <div class="talking case5">
+            <div class="bubble ">J'ai acquis une certaine facilité à me mêler d'affaires qui ne me regarde pas...</div>
+            <div class="images_tintin">
+                <img src="./img/Tintin_debout.png" class="tintinId" alt="Tintin salue">
+                <img src="./img/dupont.jpeg" alt="Tintin est curieux" class="curious_tintin">
+            </div>
+        </div>
+        <div class="border borderB borderL borderT planche2 case6"></div>
+
+        <div class="border borderR borderT borderB planche5 case7"></div>
+        <div class="job case8">Pilote en tout genre
+            <ul>
+                <li>date</li>
+                <li>lieu</li>
+            </ul>
+        </div>
+        <div class="border borderB borderL borderT planche6 case9"></div>
+
+        <div class="border borderR borderT borderB planche3 case10"></div>
+        <div class="talking case11">
+            <div class="bubble">Sous-marin, avion , moto, jeep, chameau, fusée...Même sans permis, je le conduis !</div>
+            <img src="./img/Tintin_debout.png" class="tintinId reverse" alt="Tintin salue">
+        </div>
+        <div class="border borderB borderL borderT planche4 case12"></div>
+
+        <div class="border borderR borderT borderB planche2 case13"></div>
+        <div class="job case14">Traficant de drogue
+            <ul>
+                <li>date</li>
+                <li>lieu</li>
+            </ul>
+        </div>
+        <div class="border borderB borderL borderT planche1 cse15"></div>
+
+        <div class="border borderR borderT borderB planche5 case16"></div>
+        <div class="talking case17">
+            <div class="bubble ">J'ai développé mon réseau de contact professionnel jusqu'en Colombie</div>
+            <div class="images_tintin">
+                <img src="./img/Tintin_debout.png" class="tintinId" alt="Tintin salue">
+                <img src="./img/Champignon.png" alt="Un champignon rouge et blanc" class="mushroom">
+            </div>
+        </div>
+        <div class="border borderB borderL borderT planche6 case18"></div>
+
+        <div class="border borderR borderT borderB planche3 case19"></div>
+        <div class="job cas20">Roi Maya
+            <ul>
+                <li>date</li>
+                <li>lieu</li>
+            </ul>
+        </div>
+        <div class="border borderB borderL borderT planche2 case21"></div>
+
+        <div class="border borderR borderT borderB planche1 case22"></div>
+        <div class="talking case23">
+            <div class="bubble">J'ai développé ma capacité managériale et mon sens de l'autorité.</div>
+            <img src="./img/Tintin_debout.png" class="tintinId reverse" alt="Tintin salue">
+        </div>
+        <div class="border borderB borderL borderT planche4 case24"></div>
 
 
-
-<div class="vignettes">
-    <div ></div>
-    <div ></div> 
-    <div ></div>
-
-    <div class="border borderR borderT borderB planche4"></div>
-    <div class="job">Journaliste indépendant
-        <ul>
-            <li>depuis 1929</li>
-            <li>partout dans le monde</li>
-        </ul>
     </div>
-    <div class="border borderB borderL borderT planche3"></div>
- 
-    <div class="border borderR borderT borderB planche1"></div>
-    <div class="talking">
-        <div class="bubble ">J'ai acquis une certaine facilité à me mêler d'affaires qui ne me regarde pas...</div> 
-        <div class="images_tintin">
-            <img src="./img/Tintin_debout.png" class="tintinId" alt="Tintin salue">
-            <div class="mushroom"></div>
-        </div>   
-    </div>
-    <div class="border borderB borderL borderT planche2"></div>
-
-    <div class="border borderR borderT borderB planche5"></div>
-    <div class="job">Pilote en tout genre
-        <ul>
-            <li>date</li>
-            <li>lieu</li>
-        </ul>
-    </div>
-    <div class="border borderB borderL borderT planche6"></div>
-
-    <div class="border borderR borderT borderB planche3"></div>
-    <div class="talking">
-        <div class="bubble">Sous-marin, avion , moto, jeep, chameau, fusée...Même sans permis, je le conduis !</div>
-        <img src="./img/Tintin_debout.png" class="tintinId reverse" alt="Tintin salue">
-    </div>
-    <div class="border borderB borderL borderT planche4"></div>
-
-    <div class="border borderR borderT borderB planche2"></div>
-    <div class="job">Traficant de drogue
-        <ul>
-            <li>date</li>
-            <li>lieu</li>
-        </ul>
-    </div>
-    <div class="border borderB borderL borderT planche1"></div>
-
-    <div class="border borderR borderT borderB planche5"></div>
-    <div class="talking">
-        <div class="bubble ">J'ai développé mon réseau de contact professionnel jusqu'en Colombie</div>       
-        <img src="./img/Tintin_debout.png" class="tintinId" alt="Tintin salue">
-    </div>
-    <div class="border borderB borderL borderT planche6"></div>
-
-    <div class="border borderR borderT borderB planche3"></div>
-    <div class="job">Roi Maya
-        <ul>
-            <li>date</li>
-            <li>lieu</li>
-        </ul>
-    </div>
-    <div class="border borderB borderL borderT planche2"></div>
-
-    <div class="border borderR borderT borderB planche1"></div>
-    <div class="talking">
-        <div class="bubble">J'ai développé ma capacité managériale et mon sens de l'autorité.</div>
-        <img src="./img/Tintin_debout.png" class="tintinId reverse" alt="Tintin salue">
-    </div>
-    <div class="border borderB borderL borderT planche4"></div>
-
-
-</div>
-<!--  ci-dessous à effacer  -->
+    <!--  ci-dessous à effacer  -->
 
 </body>
+
 </html>
