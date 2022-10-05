fa9950fd47401c3e8509a9b7f9fcdb4319423462
cedriclv <cedriclevalegant@gmail.com>
2022-10-05 14:57:27 +0200

    udpate of skills

---
+++ skillsArchive.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skills</title>
+    <link rel="stylesheet" href="root.css">
+    <link rel="stylesheet" href="navbar.css">
+    <link rel="stylesheet" href="skills.css">
+</head>
+
+<body>
+    <?php include_once("navbar.php"); ?>
+    <div class="hardAndSoftContainer">
+        <div class="skillsContainer softsSkillsContainer">
+            <div class="skillsComicsBox">
+                <div class="skillsTitleANDskillsPicture">
+                    <h2 class="skillsTitle">SOFT SKILLS</h2>
+                    <img class="imgLogoSoft" src="./img/id-tintin.png" alt="space ship" srcset="">
+                </div>
+                <div class="skillsText">
+                    <div class=" bubble skillsItems skillsItems1 ">
+                        <h3>Autonomie</h3>
+                        <div class="skillsDetails">Lorem ias roots in a piece of classical Latin iece of classical Latin iece of classical Latin iece of classical Latin iece of classical Latin literature from
+                            a treatise on the theo
+                        </div>
+                    </div>
+                    <div class="bubble skillsItems skillsItems2 ">
+                        <h3>Esprit d'equipe</h3>
+                        <div class="skillsDetails">Lorem ias roots in a piece of classical Latin literature from
+                            a treatise on the theo
+                        </div>
+                    </div>
+                    <div class="bubble skillsItems skillsItems3 ">
+                        <h3>Curieux</h3>
+                        <div class="skillsDetails">Lorem ias roots in a piece of piece of piece of piece of classical Latin literature from
+                            a treatise on the theo
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="skillsContainer hardSkillsContainer">
+            <div class="skillsComicsBox">
+                <div class="skillsTitleANDskillsPicture">
+                    <h2 class="skillsTitle">HARD SKILLS</h2>
+                    <img class="imgLogoHard" src="./img/spaceShipFlat.png" alt="space ship" srcset="">
+                </div>
+                <div class="skillsText">
+                    <div class="bubble skillsItems skillsItems2 ">
+                        <h3>HTML/CSS</h3>
+                        <div class="skillsDetails">Lorem ias roots in a piece of classical Latin literature from
+                            a treatise on the theo
+                        </div>
+                    </div>
+                    <div class="bubble skillsItems skillsItems3 ">
+                        <h3>Node JS</h3>
+                        <div class="skillsDetails">Lorem ias roots in a piece of classical Latin literature from
+                            a treatise on the theo
+                        </div>
+                    </div>
+                    <div class="bubble skillsItems skillsItems4 ">
+                        <h3>PHP / Symphony</h3>
+                        <div class="skillsDetails">Lorem ias roots in a piece of classical Latin literature from
+                            a treatise on the theo
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="tempoFoot">tempoFoot</div>
+</body>
+
+</html>
